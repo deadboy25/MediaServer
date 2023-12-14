b206ac44162ef6834e877cb175113326ccd9648d
Null <83357729+deadboy25@users.noreply.github.com>
2023-12-14 07:56:40 -0500

    Update update.sh
    Added command to update list of VPN servers available for connection

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#This pulls the latest images for the containers and restarts them
+# This pulls the latest images for the containers and restarts them
 #
-#It can be run periodically from a cronjob to keep containers up to date,
-#but must be run as root or with sudo
+# It can be run periodically from a cronjob to keep containers up to date,
+# but must be run as root or with sudo
 
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     echo "This script must be run as root or using sudo,"
@@ -13,6 +13,9 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     echo ""
     exit
 fi
+
+echo "Updating list of VPN servers"
+docker run --rm -v ./gluetun:/gluetun qmcgaw/gluetun update -enduser -providers slickvpn
 
 echo "Pulling latest images..."
 docker compose pull
